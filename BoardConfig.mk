e6d482bb4680d6b47cce4de8f203e5ebbed4e1f4
dianlujitao <dianlujitao@lineageos.org>
2019-01-31 22:26:17 +0800

    vince: Use libcryptfs_hw from commonsys repo
    Change-Id: I342f57e9e66cd4f6674d7ce318a2a8dabebef8c2
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,7 @@ TARGET_BOOTLOADER_BOARD_NAME := msm8953
 TARGET_NO_BOOTLOADER := true
 
 # Crypto
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
 # File systems
